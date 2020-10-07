c16776fff148d97ed332bfb4de3d4d92e6b8f048
DannyCoco <72511504+DannyCoco@users.noreply.github.com>
2020-10-07 08:18:36 -0700

    Initial Commit

---
+++ Addup/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Addup
+//
+//  Created by Danny Cardenas on 9/29/20.
+//
+
+#include <iostream>
+#include <any>
+using namespace std;
+
+int addUp(int num) {
+    int sum = 0;
+    for (int i = 1; i <= num; i++) {
+        sum = sum + i;
+    }
+    return sum;
+  /* OR
+   if (num == 1)
+        return 1;
+    return num + addUp(num - 1);  */
+}
+
+int main(int argc, const char * argv[]) {
+   cout << addUp(4) << endl;
+    return 0;
+}
